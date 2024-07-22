4aeb2ba39fdff263dd551a81bed3674c8b1cf35d
Prajwal Gopal Poojary <prajwalgpy@gmail.com>
2024-07-22 19:28:22 +0530

    ep-2

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export let App = () => {
+  return (
+    <div>
+      <div>Goooooo</div>
+      <div>
+        Goooooo
+        <div>Goooooo</div>
+      </div>
+    </div>
+  );
+};
