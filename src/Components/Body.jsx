8755a01380f480fd76f8949112c2ae4634a3bcd3
Prajwal Gopal Poojary <prajwalgpy@gmail.com>
2024-07-23 21:19:08 +0530

    ep-5

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { goo } from "../utils/resCardsItems";
 import RestList from "./Reastorent";
 
 const Body = () => {
+  let [fillter, setFileter] = useState(goo);
+
+  function fill() {
+    let fillter1 = goo.filter((item) => {
+      return item.info.avgRating > 4;
+    });
+    setFileter(fillter1);
+  }
+  function cooo() {
+    setFileter(goo);
+  }
+  console.log(fillter);
   return (
     <div className="main-contianer">
       <div className="search">
-        <input type="text" id="search-bar" />
-        <label htmlFor="search-bar">Search</label>
+        {/* <input type="text" id="search-bar" />
+        <label htmlFor="search-bar">Search</label> */}
+        <button onClick={fill}>top rated Reastorent</button>
+        <button onClick={cooo}>Reset</button>
       </div>
       <div className="resContiner">
-        {goo.map((item) => {
+        {fillter.map((item) => {
           return <RestList data={item} key={item.info.id} />;
         })}
       </div>
     </div>
   );
 };
+
 export default Body;
