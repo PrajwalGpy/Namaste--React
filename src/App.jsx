da8dfb946f1ced8e483ee8c241b4287110e1bd63
Prajwal Gopal Poojary <prajwalgpy@gmail.com>
2024-07-23 09:47:56 +0530

    ep 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+
+let Gooo = () => {
+  let [color, setColor] = useState(true);
+
+  function click() {
+    setColor((prev) => !prev);
+  }
+
+  return (
+    <div>
+      <h1
+        className="gooo"
+        style={{ background: color ? "red" : " green " }}
+        onClick={click}
+      >
+        What is your name
+      </h1>
+    </div>
+  );
+};
 
 export let App = () => {
   return (
     <div>
+      <Gooo />
       <div>Goooooo</div>
       <div>
         Goooooo
